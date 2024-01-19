02271955999d11868507aafe18190713f3a8bd29
Perez Githumbi <codakniggz145@gmail.com>
2024-01-19 21:24:57 +0300

    Implemented trait for ease of managing responses app wide

--- app/Traits/HttpResponses.php
+++ app/Traits/HttpResponses.php
@@ -2,26 +2,35 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Facades\App;
+
 Trait HttpResponses{
-    protected function success($data, $message = null, $code = 200){
+    protected function success($data, $message = "Operation successful", $code = 200){
         return response()->json([
              "status" => "request success",
+             "code" => $code,
              "message" => $message,
              "data" => $data,
              ], $code);
     }
 
-    protected function error($data, $message = null, $code = 500){
-        return response()->json([
-             "status" => "Error",
-             "message" => $message,
-             "data" => $data,
-             ], $code);
+    protected function error($data, $message = null, $code = 500, $trace = null){
+        $common = ["status" => "Error",
+                    "code"=> $code,
+                    "message" => $message,
+                    "data" => $data,
+                ];
+        if (App::environment("production")){
+            return response()->json($common, $code);
+        }else{
+            return response()->json([...$common, 'stackTrace' => $trace], $code);
+        }
     }
 
     protected function redirect($data, $message = null, $code = 200){
         return response()->json([
              "status" => "request success",
+            "code"=> $code,
              "message" => $message,
              "data" => $data,
              ], $code);
