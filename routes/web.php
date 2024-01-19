7f1a58773a03f2b8934ef8123c2adbdafb10600f
Perez Githumbi <codakniggz145@gmail.com>
2024-01-19 21:31:42 +0300

    Installed telescope and debugbar and registered service providers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use Barryvdh\Debugbar\Facades\Debugbar;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    Debugbar::info("INFO!");
     return view('welcome');
 });
