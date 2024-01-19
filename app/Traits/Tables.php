74a093bf886fa656d5d3205bd1901bdd8b2fc8d9
Perez Githumbi <codakniggz145@gmail.com>
2024-01-19 18:47:50 +0300

    Created Trait with replica of db model and column names for DRY

---
+++ app/Traits/Tables.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Traits;
+
+Trait Tables{
+
+    public $tableId = 'id';
+    public $bookId = 'id';
+    public $bookAvailable = 'available';
+
+
+    public $users = 'users';
+    public $userId = 'userId';
+
+
+    public $loanId = "id";
+    public $loanBook = 'book_id';
+    public $loanUser = 'user_id';
+    public $loanStatus = "loan_status";
+    public $loanDate = "loan_date";
+    public $loanDueDate = "due_date";
+    public $loanReturnDate = "return_date";
+
+
+
+    public $penaltiesId = "id";
+    public $penaltiesLoan = 'book_loan_id';
+    public $penaltiesAmount = 'penalty_amount_ksh';
+    public $penaltiesAmountPaid = 'penalty_amount_paid_ksh';
+    
+    
+    public $extensionsId = "id";
+    public $extensionsLoan = 'book_loan_id';
+    public $extensionsReturnDate = 'expected_return_date';
+    public $extensionsPenalty = 'penalty_id';
+
+    public $bookCategory = 'category_id';
+    public $categoryId = 'id';
+    public $categoryName = 'category_name';
+    public $categoryCategory = 'super_category';
+    
+
+    public function usersTable() {
+        return "users";
+    }
+    public function categoriesTable() { return "categories";}
+    public function booksTable() { return "books";}
+    public function bookLoansTable() { return "book_loans";}
+    public function extensionsTable() { return "extensions";}
+    public function penaltiesTable() { return "penalties";}
+}
